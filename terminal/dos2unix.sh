739b513b1baee4c60198e797ae75f31b8917f943
Sergey Tekuchev <tsc-targitaj@yandex.ru>
2022-12-07 01:33:35 +0300

    Commit

--- terminal/dos2unix.sh
+++ terminal/dos2unix.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
+echo "Working alias dos2unix for you..."
+grep "alias dos2unix" ~/.bashrc 2>&1 >/dev/null && echo "You already have it." && exit 0
 echo 'alias dos2unix="sed -i -e 's/'\"\$(printf '\015')\"'//g' "' | tee -a ~/.bashrc
 echo "Added alias dos2unix to file ~/.bashrc"
 echo "Usage: dos2unix file"
